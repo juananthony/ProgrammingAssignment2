caf457a1a11f70fb289b06979c2a5570161ab117
Juan Antonio Jiménez <sistemas@vincapervinka.com>
2017-01-29 14:04:19 +0100

    Creating makeCacheMatrix class
    Added functions to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a Special Matrix class
+## with cached inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) i <<- inv
+    getInverse <- function() i
+    list(set = set, get=get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+    
 }
 
 
